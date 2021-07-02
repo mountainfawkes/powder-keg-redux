78f73416eab08bebf3892d5fc01ed63dc944aea4
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-02 13:36:38 -0700

    adds setUSD function to localize currency

--- src/Main/Keg.js
+++ src/Main/Keg.js
@@ -1,16 +1,45 @@
+import PropTypes from 'prop-types'
+
 const Keg = ({ keg }) => {
-  const { name, brand, unitPrice, kegPrice, kegQuant, inventory, type } = keg
+  const {
+    name,
+    brand,
+    unitPrice,
+    kegPrice,
+    kegQuant,
+    inventory,
+    type,
+    description,
+  } = keg
+
+  const setUSD = val => {
+    const obj = { style: `currency`, currency: `USD` }
+    return val.toLocaleString(`en-US`, obj)
+  }
+
   return (
     <div>
-      <p>{name}</p>
+      <h3>{name}</h3>
       <p>{brand}</p>
-      <p>{unitPrice}</p>
-      <p>{inventory}</p>
-      <p>{kegPrice}</p>
-      <p>{kegQuant}</p>
-      <p>{type}</p>
+      <p>Price: {setUSD(unitPrice)}</p>
+      <p>In stock: {inventory}</p>
+      <p>Keg price: {setUSD(kegPrice)}</p>
+      <p>{description}</p>
+      <p>Ordnances per keg: {kegQuant}</p>
+      <p>Ordnance type: {type}</p>
     </div>
   )
 }
 
 export default Keg
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  unitPrice: PropTypes.number,
+  inventory: PropTypes.number,
+  kegPrice: PropTypes.number,
+  kegQuant: PropTypes.number,
+  type: PropTypes.string,
+  description: PropTypes.string,
+}
