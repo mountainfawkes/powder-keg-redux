cf41c45b87e1cede8bbb4fce80b7e62a25a6e927
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-02 16:10:18 -0700

    WIP adds dynamic details view

--- src/Main/KegDetail.js
+++ src/Main/KegDetail.js
@@ -1,3 +1,31 @@
-export default () => (
-  <p>This is a keg's details</p>
-)
+import Keg from './Keg'
+
+export default ({ selectedKeg }) => {
+  const {
+    name,
+    brand,
+    unitPrice,
+    kegPrice,
+    kegQuant,
+    inventory,
+    type,
+    id,
+    description,
+  } = selectedKeg
+
+  const setUSD = val => {
+    const validVal = parseFloat(val)
+    const obj = { style: `currency`, currency: `USD` }
+    return validVal.toLocaleString(`en-US`, obj)
+  }
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <p>{brand}</p>
+      <span>Price: {setUSD(unitPrice)}</span>
+      <span>{inventory}</span>
+      <p>{description}</p>
+    </>
+  )
+}
