e50be70bded87ac28331a6ae0f0e6353b01c53fd
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-03 12:31:44 -0700

    scaffolds inventory updating files

--- src/Main/Inventory/SellKeg.js
+++ src/Main/Inventory/SellKeg.js
@@ -1,3 +1,3 @@
 export default () => (
-  <button type='button'>Add</button>
+  <button type='button'>SELL KEG</button>
 )
---
+++ src/Main/Inventory/SellOrd.js
@@ -0,0 +1,3 @@
+export default () => (
+  <button type='button'>SELL ORD</button>
+)
