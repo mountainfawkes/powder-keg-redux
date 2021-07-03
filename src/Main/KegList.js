70a825b6201414f2ae6253d03cae74617dd05125
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-03 12:33:58 -0700

    refactors inventory section to appear with each keg

--- src/Main/KegList.js
+++ src/Main/KegList.js
@@ -4,13 +4,15 @@ import Keg from "./Keg"
 export default ({ mainKegList, handleKegSelection }) => (
   <>
     {mainKegList.map(keg => (
-      <Keg
-        handleKegSelection={handleKegSelection}
-        keg={keg}
-        id={keg.id}
-        key={keg.id}
-      />
+      <div>
+        <Keg
+          handleKegSelection={handleKegSelection}
+          keg={keg}
+          id={keg.id}
+          key={keg.id}
+        />
+        <Inventory />
+      </div>
     ))}
-    <Inventory />
   </>
 )
