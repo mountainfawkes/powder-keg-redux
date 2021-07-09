e26069b6c0bc4197b6f28d6b15248625da88c1b7
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-09 08:32:40 -0700

    preps for redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { Provider } from 'react-redux'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </>,
   document.getElementById(`root`)
 )
