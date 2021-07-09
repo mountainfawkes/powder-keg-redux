3f6ec84c23c82b8c7d08c8810afa0fc9705994a8
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-09 09:10:44 -0700

    passes red state, test 2, add keg

--- src/Reducers/keg-list-reducer.js
+++ src/Reducers/keg-list-reducer.js
@@ -1,1 +1,16 @@
-export default (state = {}) => state
+export default (state = {}) =>
+// const { name,
+//   brand,
+//   description,
+//   unitPrice,
+//   inventory,
+//   kegPrice,
+//   kegQuant,
+//   type,
+//   id } = action
+
+  state
+
+// add a keg
+// select and return a keg
+// select and increment a keg
--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,59 @@
 import kegListReducer from '../../Reducers/keg-list-reducer'
 
 describe(`kegListReducer`, () => {
+  let action
+  const kegData = {
+    name: `Bahamut ZERO`,
+    brand: `Cacodemonic Cacophonies`,
+    // eslint-disable-next-line max-len
+    description: `Beware the fireworks summoned from the heavens. This display renders an intergalactic laser dragon that swoops low and close to any crowd it encounters. Use in high winds for best effect. Max height is unlimited given the display's stellar origin. Minimum height: 50 feet.`,
+    unitPrice: 75,
+    inventory: 53,
+    kegPrice: 650,
+    kegQuant: 10,
+    type: `crossette`,
+    id: 6,
+  }
+
   test(`Should return the default state if there is no action`, () => {
     expect(kegListReducer({}, { type: null })).toEqual({})
+  })
+
+  test(`Should add a keg to the keg list`, () => {
+    const { name,
+      brand,
+      description,
+      unitPrice,
+      inventory,
+      kegPrice,
+      kegQuant,
+      type,
+      id } = kegData
+    action = {
+      actionType: `ADD_KEG`,
+      name,
+      brand,
+      description,
+      unitPrice,
+      inventory,
+      kegPrice,
+      kegQuant,
+      type,
+      id,
+    }
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name,
+        brand,
+        description,
+        unitPrice,
+        inventory,
+        kegPrice,
+        kegQuant,
+        type,
+        id,
+      },
+    })
   })
 })
